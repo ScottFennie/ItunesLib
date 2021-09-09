217440462a2e9281817b1bca1862d2c5f6cdd545
Scott Fennie <s.fennie7@gmail.com>
2021-09-09 16:02:19 -0600

    made layout

--- app/Controllers/SongsController.js
+++ app/Controllers/SongsController.js
@@ -1,38 +1,48 @@
+import { ProxyState } from "../AppState.js";
 import songService from "../Services/SongsService.js";
 
 //Private
 /**Draws the Search results to the page */
-function _drawResults() { }
+function _drawResults() {
+
+    console.log('draw songs', ProxyState.songs)
+    let template = ''
+    ProxyState.songs.forEach(s => template += s.Template)
+    document.getElementById('songs').innerHTML = template
+
+
+}
 
 /**Draws the Users saved songs to the page */
-function _drawPlaylist() { }
+function _drawPlaylist() {}
 
 //Public
 export default class SongsController {
-  constructor() {
-    //TODO Don't forget to register your listeners and get your data
-  }
-
-  /**Takes in the form submission event and sends the query to the service */
-  search(e) {
-    //NOTE You dont need to change this method
-    e.preventDefault();
-    try {
-      songService.getMusicByQuery(e.target.query.value);
-    } catch (error) {
-      console.error(error);
+    constructor() {
+        //TODO Don't forget to register your listeners and get your data
+        ProxyState.on('songs', _drawResults)
     }
-  }
 
-  /**
-   * Takes in a song id and sends it to the service in order to add it to the users playlist
-   * @param {string} id
-   */
-  addSong(id) { }
+    /**Takes in the form submission event and sends the query to the service */
+    search(e) {
+        //NOTE You dont need to change this method
+        e.preventDefault();
+        try {
+            songService.getMusicByQuery(e.target.query.value);
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
-  /**
-   * Takes in a song id to be removed from the users playlist and sends it to the server
-   * @param {string} id
-   */
-  removeSong(id) { }
+    /**
+     * Takes in a song id and sends it to the service in order to add it to the users playlist
+     * @param {string} id
+     */
+    addSong(id) {}
+
+    /**
+     * Takes in a song id to be removed from the users playlist and sends it to the server
+     * @param {string} id
+     */
+    removeSong(id) {}
 }
--- app/Models/Song.js
+++ app/Models/Song.js
@@ -1,24 +1,32 @@
 export default class Song {
-  constructor(data) {
-    this.title = data.trackName || data.title;
-    this.albumArt =
-      data.albumArt || data.artworkUrl100.replace(/100x100/g, "300x300");
-    this.artist = data.artistName || data.artist;
-    this.album = data.collectionName || data.album;
-    this.price = data.trackPrice || data.price;
-    this.preview = data.previewUrl || data.preview;
-    this._id = data.trackId || data._id;
-  }
+    constructor(data) {
+        this.title = data.trackName || data.title;
+        this.albumArt =
+            data.albumArt || data.artworkUrl100.replace(/100x100/g, "300x300");
+        this.artist = data.artistName || data.artist;
+        this.album = data.collectionName || data.album;
+        this.price = data.trackPrice || data.price;
+        this.preview = data.previewUrl || data.preview;
+        this._id = data.trackId || data._id;
+    }
 
-  get Template() {
-    return `
+    get Template() {
+        return /*html*/ `
+        <div class="col-lg-3 card shadow text-center my-2">
+                <img src="${this.albumArt}" alt="">
+                <h5>${this.album}</h4>
+                <h6>${this.title}</h5>
+                <p>${this.artist}</p>
+                <div class="d-flex justify-content-center">
+                <button class="btn btn-primary">add to playlist</button>
+                </div>
+        </div>
+        `;
+    }
+
+    get playlistTemplate() {
+        return `
 
         `;
-  }
-
-  get playlistTemplate() {
-    return `
-
-        `;
-  }
+    }
 }
